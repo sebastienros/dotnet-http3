a3ee4f70caeb665079102ab297fc99db75c6453c
Sebastien Ros <sebastienros@gmail.com>
2021-09-27 16:03:51 -0700

    Create project

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseKestrel(k =>
+{
+    var services = k.ApplicationServices;
+
+    k.Listen(IPAddress.Any,
+        443,
+        listenOptions =>
+    {
+        // Enables HTTP/3
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+
+        // Adds a TLS certificate to the endpoint
+        listenOptions.UseHttps(httpsOptions =>
+        {
+            //httpsOptions.UseLettuceEncrypt(services);
+        });
+    });
+})
+    //.ConfigureServices(services => services.AddLettuceEncrypt())
+    ;
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
